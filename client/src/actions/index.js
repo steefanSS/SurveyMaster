5bcc6b8764f53f752610615becff75cdc81f6285
Stefan Nicic <35577786+steefanSS@users.noreply.github.com>
2018-12-24 16:34:23 +0100

    final product

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {FETCH_USER} from "./types";
+import {FETCH_USER,FETCH_SURVEYS} from "./types";
 
 export const fetchUser = () => async (dispatch)=>{
     try{
@@ -32,4 +32,9 @@ export const submitSurvey = (values, history) => async dispatch => {
 
     }
 
+};
+
+export const fetchSurveys = () => async dispatch =>{
+    const res= await axios.get('/api/surveys');
+    dispatch({type:FETCH_SURVEYS, payload:res.data})
 };
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,8 @@
  const requireLogin= require('../middlewares/requireLogin'),
-       requireCredits= require('../middlewares/requireCredits');
+       _ = require ('lodash'),
+       Path = require('path-parser'),
+       {URL} = require('url'),
+       requireCredits= require('../middlewares/requireCredits'),
        mongoose= require('mongoose'),
        Mailer= require ('../services/Mailer'),
        surveyTemplate= require ('../services/emailTemplates/surveyTemplate');
@@ -8,9 +11,46 @@
 
        module.exports = app =>{
 
-        app.get('/api/surveys/thanks',(req,res)=>{
+        app.get('/api/surveys',requireLogin, async (req,res)=>{
+           const surveys= await Survey.find({_user: req.user.id})
+               .select({recipients:false});
+
+           res.send(surveys);
+        });
+
+        app.get('/api/surveys/:surveyId/:choice',(req,res)=>{
             res.send('Thanks for voting');
         });
+
+        app.post('/api/surveys/webhooks',(req,res) =>{
+            //this way there is no need to create a new helper for every iteration through the loop
+            const p = new Path('/api/surveys/:surveyId/:choice');
+             _.chain(req.body)
+                 .map(({email,url})=>{
+                    //we take full url and only extract /api/surveys/(surveys.id)/yes(or no)
+                 const match = p.test(new URL(url).pathname);
+                 if(match){
+                     return {email,surveyId:match.surveyId,choice:match.choice}
+                 }
+                })
+                 .compact()
+                 .uniqBy('email','surveyId')
+                 .each(({surveyId, email,choice}) =>{
+                     Survey.updateOne({
+                         _id:surveyId,
+                         recipients: {
+                             $elemMatch: {email: email,responded: false}
+                         }
+                     },{
+                         $inc: {[choice]:1},
+                         $set: {'recipients.$.responded': true},
+                         lastResponded:new Date()
+                     }).exec();
+                 })
+                 .value();
+
+             res.send({});
+           });
 
         app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
                const { title, subject, body, recipients } = req.body;
@@ -23,7 +63,6 @@
                    _user: req.user.id,
                    dateSent: Date.now()
                });
-
         //Great place to send an email - we are passing an object that contains subject  and recipient property
             // as a second parameter we are sending content of the actual email 'body'
             const mailer= new Mailer(survey, surveyTemplate(survey));
